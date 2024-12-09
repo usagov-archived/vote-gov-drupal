9ec3c3c7a3c2e71b8eb071b1f027ae34b9bc99cf
tt-gsa <tyler.thompson@gsa.gov>
2024-12-09 11:14:23 -0500

    sync test pipeline related files with dev

--- scripts/pipeline/downsync-preprod.sh
+++ scripts/pipeline/downsync-preprod.sh
@@ -101,23 +101,18 @@ rm -rf restore.txt ~/.mysql latest.sql
 
 date
 
+echo "Running 'drush updb -y --no-cache-clear' on '${RESTORE_ENV}' database..."
+source $(pwd $(dirname $0))/scripts/pipeline/cloud-gov-remote-command.sh "${project}-drupal-${RESTORE_ENV}" "drush updb -y --no-cache-clear"
+
+date
+
 echo "Running 'drush cr' on '${RESTORE_ENV}' database..."
 source $(pwd $(dirname $0))/scripts/pipeline/cloud-gov-remote-command.sh "${project}-drupal-${RESTORE_ENV}" "drush cr"
 
 date
 
 echo "Running 'drush cim -y' on '${RESTORE_ENV}' database..."
-source $(pwd $(dirname $0))/scripts/pipeline/cloud-gov-remote-command.sh "${project}-drupal-${RESTORE_ENV}" "drush cim -y" >/dev/null 2>&1
-
-date
-
-echo "Running 'drush cim -y' again on '${RESTORE_ENV}' database..."
-source $(pwd $(dirname $0))/scripts/pipeline/cloud-gov-remote-command.sh "${project}-drupal-${RESTORE_ENV}" "drush cim -y" >/dev/null 2>&1
-
-date
-
-echo "Running 'drush updb -y --no-cache-clear' on '${RESTORE_ENV}' database..."
-source $(pwd $(dirname $0))/scripts/pipeline/cloud-gov-remote-command.sh "${project}-drupal-${RESTORE_ENV}" "drush updb -y --no-cache-clear"
+source $(pwd $(dirname $0))/scripts/pipeline/cloud-gov-remote-command.sh "${project}-drupal-${RESTORE_ENV}" "drush cim -y"
 
 date
 
