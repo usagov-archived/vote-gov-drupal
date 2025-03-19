79788211e8ebfe1d022c79e3960371366e22bf50
daniel crowder <daniel.crowder@gsa.gov>
2025-03-19 18:15:31 -0400

    Update downsync-preprod script to include dblog install check.

--- scripts/pipeline/downsync-preprod.sh
+++ scripts/pipeline/downsync-preprod.sh
@@ -99,4 +99,41 @@ echo "Cleaning up old connections..."
 ## Clean up.
 rm -rf restore.txt ~/.mysql latest.sql
 
+date
 
+echo "Checking if Dblog module's watchdog table should exist or not on '${RESTORE_ENV}' database..."
+source $(pwd $(dirname $0))/scripts/pipeline/cloud-gov-remote-command.sh "${project}-drupal-${RESTORE_ENV}" \
+'drush php:eval "
+if (!\Drupal::moduleHandler()->moduleExists(\"dblog\") && \Drupal::database()->schema()->tableExists(\"watchdog\")) {
+  \Drupal::database()->schema()->dropTable(\"watchdog\");
+  echo \"Dblog module not enabled - watchdog table dropped.\\n\";
+} else {
+  echo \"No action needed.\\n\";
+}"'
+
+date
+
+echo "Running 'drush cr' on '${RESTORE_ENV}' database..."
+source $(pwd $(dirname $0))/scripts/pipeline/cloud-gov-remote-command.sh "${project}-drupal-${RESTORE_ENV}" "drush cr"
+
+date
+
+echo "Running 'drush updb -y --no-cache-clear' on '${RESTORE_ENV}' database..."
+source $(pwd $(dirname $0))/scripts/pipeline/cloud-gov-remote-command.sh "${project}-drupal-${RESTORE_ENV}" "drush updb -y --no-cache-clear"
+
+date
+
+echo "Running 'drush cim -y' on '${RESTORE_ENV}' database..."
+source $(pwd $(dirname $0))/scripts/pipeline/cloud-gov-remote-command.sh "${project}-drupal-${RESTORE_ENV}" "drush cim -y"
+
+date
+
+echo "Running 'drush cr' on '${RESTORE_ENV}' database..."
+source $(pwd $(dirname $0))/scripts/pipeline/cloud-gov-remote-command.sh "${project}-drupal-${RESTORE_ENV}" "drush cr"
+
+date
+
+echo "Running 'drush image-flush --all' on '${RESTORE_ENV}'..."
+source $(pwd $(dirname $0))/scripts/pipeline/cloud-gov-remote-command.sh "${project}-drupal-${RESTORE_ENV}" "drush image-flush --all"
+
+date
